77a7b84b5012ba741cdb2d825806aea96d9cc730
PandawanFr <pandawanfr@gmail.com>
2018-10-11 08:27:05 -0700

    Finished basic motion system

--- corndog/corndog.ino
+++ corndog/corndog.ino
@@ -7,6 +7,10 @@
 int motionSensorPin = 2;
 // Value read from Motion Sensor (PIR) [LOW/HIGH]
 int motionSensorValue = LOW;
+int previousSensorValue = LOW;
+
+// TODO: Maybe add a system so that you can launch it only once and you have to do a specific step to reset it
+
  
 void setup() {
   // Declare Motion Sensor Pin as an Input
@@ -16,7 +20,18 @@ void setup() {
 }
  
 void loop(){
+  // Refresh the Motion Sensor Value
   motionSensorValue = digitalRead(motionSensorPin);
-  Serial.print("Variable: ");
-  Serial.println(motionSensorValue);
+  // If it switched from LOW to HIGH
+  if (motionSensorValue == HIGH && previousSensorValue == LOW) {
+    // Launch the facehugger
+    launch();
+  }
+  // Set the previous value
+  previousSensorValue = motionSensorValue;
+}
+
+void launch() {
+  // TODO: Make launching system using motor
+  Serial.println("Launch facehugger!");
 }
