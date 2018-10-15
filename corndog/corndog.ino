7af0bf7380f60c1203bb1d407b251f49493949be
PandawanFr <pandawanfr@gmail.com>
2018-10-15 09:15:17 -0700

    Idk, it should work but didn't try it

--- corndog/corndog.ino
+++ corndog/corndog.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 /**
    PROJECT CORNDOG
    It's not a fruit.
@@ -22,76 +24,106 @@ int statusLightPinRed = 9;
 int statusLightPinGreen = 10;
 int statusLightPinBlue = 11;
 
-void setup() {
-  // Declare Motion Sensor Pin as an Input
-  pinMode(motionSensorPin, INPUT);
-  
-  // Declare Button as an Input
-  pinMode(readyButtonPin, INPUT);
+// Servo
+int servoPin = A0;
+Servo servo;
 
-  // Declare Status Light as Output
-  pinMode(statusLightPinRed, OUTPUT);
-  pinMode(statusLightPinGreen, OUTPUT);
-  pinMode(statusLightPinBlue, OUTPUT);
+void setup()
+{
+	// Declare Motion Sensor Pin as an Input
+	pinMode(motionSensorPin, INPUT);
 
-  Serial.begin(9600);
+	// Declare Button as an Input
+	pinMode(readyButtonPin, INPUT);
+
+	// Declare Status Light as Output
+	pinMode(statusLightPinRed, OUTPUT);
+	pinMode(statusLightPinGreen, OUTPUT);
+	pinMode(statusLightPinBlue, OUTPUT);
+
+	// Motion Sensor
+	servo.attach(servoPin);
+
+	Serial.begin(9600);
 }
 
-void loop() {
-  readyStateRefresh();
-  statusLightRefresh();
-  motionSensorRefresh();
+void loop()
+{
+	readyStateRefresh();
+	statusLightRefresh();
+	motionSensorRefresh();
 }
 
 // Refrehs the motion sensor's value and do launch system
-void motionSensorRefresh() {
-  // Refresh the Motion Sensor Value
-  motionSensorValue = digitalRead(motionSensorPin);
-  // If it is ready for launch
-  if (readyForLaunch == HIGH) {
-    // If it switched from LOW to HIGH
-    if (motionSensorValue == HIGH && previousSensorValue == LOW) {
-      // Launch the facehugger
-      launch();
-    }
-  }
-  // Set the previous value
-  previousSensorValue = motionSensorValue;
+void motionSensorRefresh()
+{
+	// Refresh the Motion Sensor Value
+	motionSensorValue = digitalRead(motionSensorPin);
+	// If it is ready for launch
+	if (readyForLaunch == HIGH)
+	{
+		// If it switched from LOW to HIGH
+		if (motionSensorValue == HIGH && previousSensorValue == LOW)
+		{
+			// Launch the facehugger
+			launch();
+		}
+	}
+	// Set the previous value
+	previousSensorValue = motionSensorValue;
 }
 
 // Ready State controlled by a button
-void readyStateRefresh() {
-  readyButtonValue = digitalRead(readyButtonPin);
+void readyStateRefresh()
+{
+	readyButtonValue = digitalRead(readyButtonPin);
 
-  if (readyButtonValue == HIGH) {
-    readyForLaunch = HIGH;
-  }
+	if (readyButtonValue == HIGH)
+	{
+		reset();
+	}
 }
 
-void statusLightRefresh() {
-  if (readyForLaunch == LOW) {
-    setColor(statusLightPinRed, 255, statusLightPinGreen, 0, statusLightPinBlue, 0);
-  }
-  else if (readyForLaunch == HIGH) {
-    setColor(statusLightPinRed, 0, statusLightPinGreen, 255, statusLightPinBlue, 0);
-  }
+// Refresh the light color
+void statusLightRefresh()
+{
+	if (readyForLaunch == LOW)
+	{
+		setColor(statusLightPinRed, 255, statusLightPinGreen, 0, statusLightPinBlue, 0);
+	}
+	else if (readyForLaunch == HIGH)
+	{
+		setColor(statusLightPinRed, 0, statusLightPinGreen, 255, statusLightPinBlue, 0);
+	}
 }
 
-void launch() {
-  // TODO: Make launching system using motor
-  Serial.println("Launch facehugger!");
-  readyForLaunch = LOW;
+// Launch the facehugger
+void launch()
+{
+	// TODO: Make launching system using motor
+	Serial.println("Launch facehugger!");
+
+	servo.write(180);
+
+	readyForLaunch = LOW;
+}
+
+// Reset properties so everything is ready for launch
+void reset()
+{
+	servo.write(0);
+	readyForLaunch = HIGH;
 }
 
 // Set Color Utility
 void setColor(int redPin, int red, int greenPin, int green, int bluePin, int blue)
 {
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);  
+#ifdef COMMON_ANODE
+	red = 255 - red;
+	green = 255 - green;
+	blue = 255 - blue;
+#endif
+	analogWrite(redPin, red);
+	analogWrite(greenPin, green);
+	analogWrite(bluePin, blue);
 }
